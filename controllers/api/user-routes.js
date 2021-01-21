96fd6ef0b66659dd69ac3b33ba6e3f8d0a54a89b
unknown <peters0470@gmail.com>
2021-01-21 06:59:06 -0600

    added get all route to users api and adjusted model for user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,4 +1,30 @@
 const router = require('express').Router();
-const db = require('../../models')
+
+const { User } = require('../../models'); 
+
+
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] }
+      })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  // GET /api/users/1
+router.get('/:id', (req, res) => {});
+
+// POST /api/users
+router.post('/', (req, res) => {});
+
+// PUT /api/users/1
+router.put('/:id', (req, res) => {});
+
+// DELETE /api/users/1
+router.delete('/:id', (req, res) => {});
+
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -6,5 +6,5 @@ router.use('/api', apiRoutes);
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
 });
-
+ 
 module.exports = router;
