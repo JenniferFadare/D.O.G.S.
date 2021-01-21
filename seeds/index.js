c21ba71bde6b6e68179e188b4cb4c977a2ceb1ad
unknown <peters0470@gmail.com>
2021-01-20 21:18:03 -0600

    restructured seeds to work properly

--- seeds/index.js
+++ seeds/index.js
@@ -8,15 +8,12 @@ const sequelize = require('../config/connection');
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
-  await dog();
-  console.log('\n----- DOG SEEDED -----\n');
-
-  await topDog();
-  console.log('\n----- TOPDOG SEEDED -----\n');
-
   await user();
   console.log('\n----- USER SEEDED -----\n');
-
+  await dog();
+  console.log('\n----- DOG SEEDED -----\n');
+  await topDog();
+  console.log('\n----- TOPDOG SEEDED -----\n');
   process.exit(0);
 };
 
