1354fa0cdf68d19ab59fb1664380e41e39539faa
Casey Deriso <caseyderiso@gmail.com>
2021-01-22 14:42:12 -0600

    created form handler for adding new dog

---
+++ public/javascript/add-dog.js
@@ -0,0 +1,35 @@
+async function addDogFormHandler(event) {
+  event.preventDefault();
+
+  // snag user_id from the URL (should be app.com/users/1 => 1)
+  const user_id = document.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+  const name = document.querySelector("#dog-name").value.trim();
+  const breed = document.querySelector("#dog-breed").value.trim();
+  const temperament = document.querySelector("#dog-temperament").value.trim();
+
+  if (user_id && name && breed && temperament) {
+    const response = await fetch("/dogs", {
+      method: "post",
+      body: {
+        user_id,
+        name,
+        breed,
+        temperament,
+      },
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      console.log("success");
+      document.location.reload();
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document
+  .querySelector(".dog-form")
+  .addEventListener("submit", addDogFormHandler);
