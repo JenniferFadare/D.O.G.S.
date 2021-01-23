fe149dcc84b9e86ba336fb9e51eb94cda07b0643
unknown <peters0470@gmail.com>
2021-01-22 18:20:07 -0600

    set up handlebars functionality

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
-
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
