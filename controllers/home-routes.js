92009c872af7cec6c4ee58cd8cb44c1a6fb82628
Casey Deriso <caseyderiso@gmail.com>
2021-01-26 23:04:26 -0600

    added list of all users on homepage for logged in users

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -8,6 +8,7 @@ router.get('/', (req, res) => {
   })
   .then(dbUserData => {
     const users = dbUserData.map((users) => users.get({plain: true }));
+    console.log(users)
     res.render('homepage', { users, loggedIn: req.session.loggedIn })
   })
   .catch(err => {
