307173b7d617c4e17d60086d43d6f4c7af9e85d1
Casey Deriso <caseyderiso@gmail.com>
2021-01-29 16:22:14 -0600

    built socket as per heroku dev article

--- public/javascript/chat-script.js
+++ public/javascript/chat-script.js
@@ -1,5 +1,5 @@
 // connect to the project, will need to be updated to
-const socket = io(`${document.location.href}`);
+const socket = io();
 // grab DOM elements for chat
 const messageContainer = document.querySelector("#message-container");
 const messageForm = document.querySelector("#send-container");
